e2965c99587a44f0b08776237d803353676f10f6
AytenElemli <127186240+AytenElemli@users.noreply.github.com>
2023-05-04 13:31:49 +0400

    Add files via upload

---
+++ JS-Events/assets/js/main.js
@@ -0,0 +1,22 @@
+function myFunctionLondon(){
+const london=document.getElementById("london");
+london.style.display="block";
+paris.style.display= "none";
+tokyo.style.display= "none";
+}
+
+function myFunctionParis(){
+const paris=document.getElementById("paris");
+paris.style.display= "block";
+london.style.display= "none";
+tokyo.style.display= "none";
+}
+
+function myFunctionTokyo(){
+const tokyo=document.getElementById("tokyo");
+tokyo.style.display= "block";
+paris.style.display= "none";
+london.style.display= "none";
+}
+
+
